de7452bbc25ad52fab90623a97aef909a40dcb5e
kangdongyoon7554 <kangdongyoon8111@gmail.com>
2025-09-10 11:41:13 +0900

    Create GithubTest.cs

---
+++ Assets/GithubTest.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GithubTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("깃 허브 잘 작동되나요.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
