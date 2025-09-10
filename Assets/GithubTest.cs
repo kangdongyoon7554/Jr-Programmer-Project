ed2287a41ea3ef6fa52d360d3b97f12995588561
강동윤 <davidkang8111@gmail.com>
2025-09-10 11:42:20 +0900

    202509101142_GitTest
    테스트 중입니다.

---
+++ Assets/GithubTest.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GithubTest : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("깃 허브 테스트 중입니다.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
